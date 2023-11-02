b413b841f82fd2b6ff41bcd66eedf4f028921c07
Edgar-Chmylko <echmylko@gmail.com>
2023-11-02 11:57:47 +0200

    Add Home-page component

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,11 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CountryLeagueService } from 'src/app/services/country-league.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePageComponent {
+  constructor(private countryLeagueService: CountryLeagueService) {}
 
+  onSelectedCountry(selectedCountry: string) {
+    this.countryLeagueService.loadCountryLeague(selectedCountry).subscribe((val) => console.log(val));
+  }
 }
