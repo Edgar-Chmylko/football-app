51dcc841149e5a5fec179e76aff49e3fc4ca3f90
Edgar-Chmylko <echmylko@gmail.com>
2023-11-04 19:45:43 +0200

    add environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    apiKey: '15c3e0d175msh31064e99f5ec2adp121581jsnc9cde07b494c',
+    apiHost: 'api-football-v1.p.rapidapi.com',
+    apiLeaguesUrl : 'https://api-football-v1.p.rapidapi.com/v3/leagues',
+    apiLeagueStandingsUrl :'https://api-football-v1.p.rapidapi.com/v3/standings',
+    apiTeamFixturesUrl :'https://api-football-v1.p.rapidapi.com/v3/fixtures',
+};
