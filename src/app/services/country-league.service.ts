f85e8eacfde5f0a0582056421cf9b83cc0274568
Edgar-Chmylko <echmylko@gmail.com>
2023-11-04 19:48:12 +0200

    add country league service

--- src/app/services/country-league.service.ts
+++ src/app/services/country-league.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { FootballApiService } from './football-api.service';
-import { shareReplay } from 'rxjs';
+import { BehaviorSubject, Observable, map, tap } from 'rxjs';
+import { CountryLeague, Standing } from '../shared/models/models';
+import { Country } from '../home/components/country-selector/country-selector.component';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +10,30 @@ import { shareReplay } from 'rxjs';
 export class CountryLeagueService {
   constructor(private footbalApi: FootballApiService) {}
 
-  loadCountryLeague(countryName: string) {
-    return this.footbalApi.loadCountryLeague(countryName).pipe(shareReplay());
+  private countryLeague = new BehaviorSubject<CountryLeague | null>(null);
+  private leagueStandings = new BehaviorSubject<Standing[]>([]);
+
+  countryLeague$ = this.countryLeague.asObservable();
+  leagueStandings$ = this.leagueStandings.asObservable();
+
+  loadCountryLeague(country: Country) {
+    return this.footbalApi.loadCountryLeague(country.name).pipe(
+      map((api) => {
+        return api.response.find(
+          (x) =>
+            x.country.name === country.name && x.league.name === country.league
+        );
+      }),
+      tap((countryLeague) => this.countryLeague.next(countryLeague!.league))
+    );
+  }
+
+  loadLeagueStandings(leagueId: number) {
+    return this.footbalApi.loadLeagueStandings(leagueId).pipe(
+      map((api) => {
+        return api.response[0].league.standings.flat();
+      }),
+      tap((standings) => this.leagueStandings.next(standings))
+    );
   }
 }
