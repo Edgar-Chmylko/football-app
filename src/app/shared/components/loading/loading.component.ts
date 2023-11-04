396dcf5fc9eb05800140daa685e2b28f62747266
Edgar-Chmylko <echmylko@gmail.com>
2023-11-04 19:50:27 +0200

    add loading component

---
+++ src/app/shared/components/loading/loading.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading',
+  templateUrl: './loading.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LoadingComponent {
+
+}
