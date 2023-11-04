f02b1de63bf8875b3207bf08af6a3d32b1180a31
Edgar-Chmylko <echmylko@gmail.com>
2023-11-04 19:50:57 +0200

    update app module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home/home-page/home-page.component';
-import { CountrySelectorComponent } from './home/country-selector/country-selector.component';
-import { LeagueStandingsTableComponent } from './home/league-standings-table/league-standings-table.component';
+import { HomePageComponent } from './home/pages/home-page/home-page.component';
+import { CountrySelectorComponent } from './home/components/country-selector/country-selector.component';
+import { LeagueStandingsTableComponent } from './home/components/league-standings-table/league-standings-table.component';
 
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatTableModule } from '@angular/material/table'; 
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { LoadingComponent } from './shared/components/loading/loading.component';
+import { MatchHistoryTableComponent } from './club/components/match-history/match-history-table.component';
+import { ClubPageComponent } from './club/pages/club-page/club-page.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ErrorPageComponent } from './shared/pages/error-page/error-page.component';
+import { ButtonComponent } from './shared/components/button/button.component';
 
 @NgModule({
   declarations: [
@@ -18,16 +25,23 @@ import { MatTableModule } from '@angular/material/table';
     HomePageComponent,
     CountrySelectorComponent,
     LeagueStandingsTableComponent,
+    LoadingComponent,
+    ClubPageComponent,
+    MatchHistoryTableComponent,
+    ErrorPageComponent,
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule,
     MatButtonToggleModule,
     HttpClientModule,
-    MatTableModule
+    MatTableModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
