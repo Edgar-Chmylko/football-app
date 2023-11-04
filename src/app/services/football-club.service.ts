fa5d68e4c42bda9f047b8add8d57fa84eed85d5e
Edgar-Chmylko <echmylko@gmail.com>
2023-11-04 19:48:45 +0200

    add football club service

--- src/app/services/football-club.service.ts
+++ src/app/services/football-club.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { FootballApiService } from './football-api.service';
+import { BehaviorSubject, map, tap } from 'rxjs';
+import { MatchHistory } from '../shared/models/models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FootballClubService {
-  constructor() {}
+  constructor(private footbalApi: FootballApiService) {}
+
+  private matchHistory = new BehaviorSubject<MatchHistory[]>([]);
+  matchHistory$ = this.matchHistory.asObservable();
+
+  loadMatchHistory(teamId: number, leagueId: number) {
+    return this.footbalApi.loadMatchHistory(teamId, leagueId).pipe(
+      map((api) => api.response),
+      tap((matchHistory) => this.matchHistory.next(matchHistory))
+    );
+  }
 }
