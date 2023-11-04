dbc1bd65d0683b63922a0ca84204d1950095f32f
Edgar-Chmylko <echmylko@gmail.com>
2023-11-04 19:48:34 +0200

    add api service

--- src/app/services/football-api.service.ts
+++ src/app/services/football-api.service.ts
@@ -1,19 +1,69 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http'
+import { HttpClient, HttpParams } from '@angular/common/http';
+import {
+  ApiLeagueResponse,
+  ApiMatchHistoryResponse,
+  ApiStandingsResponse,
+} from '../shared/models/models';
+import { EnvironmentService } from './environment.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FootballApiService {
-  constructor(private httpClient: HttpClient) { }
+  constructor(
+    private httpClient: HttpClient,
+    private environmentService: EnvironmentService
+  ) {}
 
-  private key = '15c3e0d175msh31064e99f5ec2adp121581jsnc9cde07b494c';
-  private host = 'api-football-v1.p.rapidapi.com';
-  private leaguesUrl = 'https://api-football-v1.p.rapidapi.com/v3/leagues';
+  private apiHeaders = {
+    'X-RapidAPI-Key': this.environmentService.apiKey,
+    'X-RapidAPI-Host': this.environmentService.apiHost,
+  };
+
+  currentYear = new Date().getFullYear();
 
   loadCountryLeague(countryName: string) {
     let httpParams = new HttpParams().append('country', countryName);
 
-    return this.httpClient.get<any>(this.leaguesUrl,{headers:{'X-RapidAPI-Key': this.key, 'X-RapidAPI-Host': this.host}, params: httpParams});
+    return this.httpClient.get<ApiLeagueResponse>(
+      this.environmentService.apiLeaguesUrl,
+      {
+        headers: this.apiHeaders,
+        params: httpParams,
+      }
+    );
+  }
+  loadLeagueStandings(leagueId: number) {
+    let httpParams = new HttpParams()
+      .append('league', leagueId)
+      .append('season', this.currentYear);
+
+    return this.httpClient.get<ApiStandingsResponse>(
+      this.environmentService.apiLeaguesStandingsUrl,
+      {
+        headers: this.apiHeaders,
+        params: httpParams,
+      }
+    );
+  }
+
+  loadMatchHistory(teamId: number, leagueId: number) {
+    let httpParams = new HttpParams()
+      .append('team', teamId)
+      .append('season', this.currentYear)
+      .append('last', 10);
+
+    if (leagueId) {
+      httpParams.append('league', leagueId);
+    }
+
+    return this.httpClient.get<ApiMatchHistoryResponse>(
+      this.environmentService.apiTeamFixturessUrl,
+      {
+        headers: this.apiHeaders,
+        params: httpParams,
+      }
+    );
   }
 }
