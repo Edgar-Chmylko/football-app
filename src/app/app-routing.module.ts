6456e19fe81f3bb7194728c80ad2ce0ab7496994
Edgar-Chmylko <echmylko@gmail.com>
2023-11-04 19:46:28 +0200

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './home/home-page/home-page.component';
+import { HomePageComponent } from './home/pages/home-page/home-page.component';
+import { ClubPageComponent } from './club/pages/club-page/club-page.component';
+import { ErrorPageComponent } from './shared/pages/error-page/error-page.component';
 
-const routes: Routes = [{
-  path: '',
-  component: HomePageComponent
-}];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'club/:name',
+    component: ClubPageComponent,
+  },
+  {
+    path: 'error',
+    component: ErrorPageComponent,
+  },
+  {
+    path: '**',
+    component: HomePageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
